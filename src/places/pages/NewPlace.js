84a41b8b6e6ac3561bbdf3fe638aeb93b6487452
Cook1619 <cook6348@gmail.com>
2019-12-05 10:54:09 -0600

    first commit

---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NewPlace() {
+    return (
+        <div>
+            <h1>New place</h1>
+        </div>
+    )
+}
---
+++ src/user/pages/Users.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Users() {
+    return (
+        <div>
+            <h2>USERS WORKS</h2>
+        </div>
+    )
+}
+
